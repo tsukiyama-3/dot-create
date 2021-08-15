900ac6e02f9103cb725f10924cafa73ce22ae3c9
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2021-08-16 01:23:09 +0900

    add analitics

---
+++ src/components/LoadDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="over-lay">
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style scoped>
+.over-lay {
+  background-color: rgba(0,0,0,0.5);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
