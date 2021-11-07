4866392116aa83bc887d40c1f3c2cdb0e9f6c89d
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2021-11-07 15:15:09 +0900

    add firebase analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { createApp } from "vue"
+import firebase from "firebase/app"
+import 'firebase/analytics'
 import App from "./App.vue"
 import router from './router/router'
 import "virtual:windi.css"
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAjq3D4De9m6_vOnFaefyqiD2tbBOJYuIg",
+  authDomain: "dot-creator.firebaseapp.com",
+  projectId: "dot-creator",
+  storageBucket: "dot-creator.appspot.com",
+  messagingSenderId: "372946048427",
+  appId: "1:372946048427:web:32a9d01a1579dd73c72eee",
+  measurementId: "G-GTL02DQBNR"
+};
+
+// console.log(firebase)
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 createApp(App).use(router).mount('#app')
